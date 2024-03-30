9078a28e2169f319bebd496573ca92680285c26d
belij2111 <belijvalerij2111@gmail.com>
2024-03-30 14:51:42 +0300

    feat(db): Add new options to the database

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,18 @@
 import {VideoDBType} from "./video-db-type";
 
 export type DBType = {
-    videos: VideoDBType[]
+    videos: any[]
+}
+
+export const db: DBType = {
+    videos: []
+}
+
+export const setDB = (dataset?: Partial<DBType>) => {
+    if (!dataset) {
+        db.videos = []
+        return
+    }
+    db.videos = dataset.videos || db.videos
 }
 

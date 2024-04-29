58bd138609f36cb0b2905ded783096b89a469d74
belij2111 <belijvalerij2111@gmail.com>
2024-04-29 22:08:43 +0300

    style: Remove spaces

--- src/testing/deleteTestingController.ts
+++ src/testing/deleteTestingController.ts
@@ -2,9 +2,9 @@ import {Request, Response} from "express";
 import {HTTP_STATUSES} from "../settings";
 import {db} from "../db/db";
 
-export const deleteTestingController = (req:Request,res:Response)=>{
-    db.videos=[]
+export const deleteTestingController = (req: Request, res: Response) => {
+    db.videos = []
     res
         .status(HTTP_STATUSES.NO_CONTENT_204)
-        .json({message:'Attention the database has been cleared'})
+        .json({message: 'Attention the database has been cleared'})
 }
